f994bcf9c568d21f7dfc2d4b99e5e69ed1d639aa
fu <yanyuan@g.ecc.u-tokyo.ac.jp>
2022-09-05 23:36:21 +0900

    initial commit

---
+++ kamo.py
@@ -0,0 +1,94 @@
+import json
+import argparse
+import schedule
+import datetime
+import time
+
+from utils import send_message, kamo_monitor
+
+def daily_test():
+    # load configure files
+    parser = argparse.ArgumentParser()
+    args2 = parser.parse_args()
+    with open('./configs/dailytest.json', "r") as config_file:
+        configs = json.load(config_file)
+    args2.__dict__.update(configs)    
+
+    daily_data = {
+        "datetime": {
+            "value": datetime.datetime.now(),
+            "color": "#173177"
+        }
+    }
+
+    send_message(args2, daily_data)
+    print('daily test done')
+
+def kamo_test(args):
+    # get last code 
+    with open("./data/data.json", "r") as f:
+        last_code = json.load(f)
+
+    lst = kamo_monitor(last_code['kamo_lastcode'])
+
+    if lst != []:
+        new_msgs = []
+        for item in lst:
+            msg = '\n'.join([item.brand, item.name, item.price])
+            new_msgs.append(msg)
+        total_msg = '\n\n'.join(new_msgs)
+
+        data = {
+            "items": {
+                "value": total_msg,
+                "color": "#173177"
+            }
+        }
+
+        send_message(args, data)
+
+        # save new last code
+        last_code['kamo_lastcode'] = lst[0].code
+        with open("./data/data.json", 'w') as f:
+            json.dump(last_code, f, indent=4)
+
+def schedule_plan(time_list):
+    schedule.clear()
+    
+    for t in time_list:
+        schedule.every().day.at(t).do(kamo_test, args).tag('kamo')
+    print(schedule.get_jobs())
+    schedule.every().day.at('08:00').do(daily_test).tag('daily_test')
+    schedule.every().day.at('23:00').do(daily_test).tag('daily_test')
+
+if __name__ == '__main__':
+    # load configure files
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', '-c',
+                            type=str,
+                            default='./configs/config.json',
+                            help='config files')
+    args = parser.parse_args()
+
+    if args.config:
+        with open(args.config, "r") as config_file:
+            configs = json.load(config_file)
+    args.__dict__.update(configs)
+
+
+    time_list = ['08:00:10', '08:01:10', '08:30:10', '08:31:10', 
+                 '09:00:10', '09:01:10', '09:30:10', '09:31:10', 
+                 '10:00:10', '10:01:10', '10:30:10', '10:31:10', 
+                 '11:00:10', '11:01:10', '11:30:10', '11:31:10', 
+                 '12:00:10', '12:01:10', '12:30:10', '12:31:10', 
+                 '13:00:10', '13:01:10', '13:30:10', '13:31:10', 
+                 '14:00:10', '14:01:10', '14:30:10', '14:31:10', 
+                 '15:00:10', '15:01:10', '15:30:10', '15:31:10', 
+                 '16:00:10', '16:01:10', '16:30:10', '16:31:10', 
+                 '17:00:10', '17:01:10', '17:30:10', '17:31:10', 
+                 '18:00:10', '18:01:10', '18:30:10', '18:31:10']
+    schedule_plan(time_list)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
---
+++ utils.py
@@ -0,0 +1,89 @@
+import wechatpy
+from wechatpy import WeChatClient, WeChatClientException
+from wechatpy.client.api import WeChatMessage
+
+import requests
+from bs4 import BeautifulSoup
+
+brands = {
+    '[ミズノ]' : 'mizuno',
+    '[ナイキ]' : 'nike',
+    '[アディダス]' : 'adidas',
+    '[プーマ]' : 'puma',
+    '[アシックス]' : 'acisc'
+}
+
+class Spike:
+    def __init__(self, name, brand, price, code=None, link=None):
+        self.name = name
+        self.brand = brand
+        self.price = price
+        self.code = code
+        self.link = link
+
+def kamo_monitor(last_code):
+    """
+    kamo new arrival monitor
+    """
+    url = "https://www.sskamo.co.jp/s/search/stdcdsd_enew/"
+    page = requests.get(url)
+    page_text = page.text
+    # print(page_test)
+
+    soup = BeautifulSoup(page_text, 'lxml')
+    goods = soup.find_all('div', class_="block-goods-list-d--item-area")
+
+    lst = []
+    for g in goods:
+        name = g.find('div', class_="block-goods-list-d--items-name")
+        brand = g.find('div', class_="block-goods-list-d--items-brand").text
+        price = g.find('div', class_="block-goods-list-d--items-price").text.replace('\t','').replace('\n', '')[1:-1]
+        code = name.a['href'][6:-1]
+        link = 'https://www.sskamo.co.jp' + name.a['href']
+        # stop if no more new items
+        if code == last_code:
+            break
+        # translate jp into en
+        brand = brands[brand] if brand in brands else brand
+
+        lst.append(Spike(name=name.text, 
+                        brand=brand,
+                        price=price,
+                        code=code,
+                        link=link))
+
+    return lst
+
+def send_message(args, data):
+    """
+    # appID
+    # appsecret
+    # templateID
+    # userID
+    # messages
+    """
+    # data format
+    # https://mp.weixin.qq.com/debug/cgi-bin/readtmpl?t=tmplmsg/faq_tmpl
+    # data = {
+    #     "items": {
+    #         "value": messages,
+    #         "color": "#173177"
+    #     }
+    # }
+
+    try:
+        client = WeChatClient(args.appID, args.appsecret)
+    except WeChatClientException as e:
+        # print('微信获取 token 失败，请检查 appID 和 appsecret, 或当日调用量是否已达到微信限制。')
+        print('failed to get token')
+        exit(502)
+
+    wm = WeChatMessage(client)
+
+    try:
+        print('Sending...')
+        res = wm.send_template(args.userID, args.templateID, data)
+        print('Successfully Sent!')
+    except WeChatClientException as e:
+        print(f'Error! Error Message: {e.errmsg} Error Code: {e.errcode}')
+        exit(502)
